dfedf8afd553aef9e29806fec7d76841cd9c1cf8
Vladislav Fedyaev <vladislav.fdv@mail.ru>
2021-11-03 21:59:45 +0300

    add SHA2

---
+++ encrypt.rb
@@ -0,0 +1,26 @@
+require "digest"
+
+puts 'Введите слово или фразу для шифрования:'
+string = STDIN.gets.chomp
+
+puts <<~WAYS
+  Каким способом зашифровать:
+  1. MD5
+  2. SHA1
+  3. SHA2
+WAYS
+
+user_choice = STDIN.gets.chomp.to_i
+
+puts
+puts 'Вот что получилось:'
+
+result =
+  case user_choice
+  when 1 then Digest::MD5.hexdigest(string)
+  when 2 then Digest::SHA1.hexdigest(string)
+  when 3 then Digest::SHA2.hexdigest(string)
+  else 'Вы не ввели цифру соответствующую нужному варианту шифрования'
+  end
+
+puts result
